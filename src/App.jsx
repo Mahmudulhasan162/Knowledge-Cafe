08a6da3c55ab9d9c7f9df4e6fe518c06ebc6e2e4
Mahmudulhasan162 <mahmudulhasanshafi2345@gmail.com>
2023-09-14 00:33:30 +0600

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
 function App() {
@@ -8,7 +9,10 @@ function App() {
     <>
      
       <Header></Header>
+      <div className='flex'>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
       
     </>
   )
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
+        <div className='flex justify-between p-10 mx-6 border-b-2'>
             <h1 className='text-5xl text-center font-bold' >Knowledge Cafe</h1>
+            <img src={profile} alt="" />
         </div>
     );
 };
