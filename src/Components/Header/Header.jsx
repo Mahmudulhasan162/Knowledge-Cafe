f8711c9fd8d135e98523becdd95abcee34fe4b8f
Mahmudulhasan162 <mahmudulhasanshafi2345@gmail.com>
2023-09-13 23:33:24 +0600

    working

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-center font-bold' >Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
